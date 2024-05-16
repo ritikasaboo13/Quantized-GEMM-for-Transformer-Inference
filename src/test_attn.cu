e0b4f8ac2564c981ee4d0733f165eabcd4fe0497
Ritika Saboo <115175278+rss9311@users.noreply.github.com>
2024-05-15 20:06:07 -0400

    adding layer norm & multihead attention

--- src/test_attn.cu
+++ src/test_attn.cu
@@ -12,6 +12,18 @@
 unsigned long long randgen_seed = 0;
 static bool on_gpu = true;
 
+
+void test_layerNorm() {
+    
+    Tensor<float> X{2, 4, true};
+    Tensor<float> Y{2, 4, true};
+    op_uniform_init(X, -3.0f, 3.0f);
+    op_layernorm(X, Y); 
+    std::cout << "X= \n" << X.str() << std::endl;
+    std::cout << "Y= \n" << Y.str() << std::endl;
+
+}
+
 void init_weights_for_test() {
         // Initialize W_q
         Index(W_q, 0, 0) = 0.2482;
@@ -49,11 +61,9 @@ void test_singleHeadAttn(const Tensor<float> X, Tensor<float> attnOutput, int d_
     // Single Head attention
     // Singlehead Attention parameters 
     // For single headed attention, d_model, d_k, d_v are all same as d_model ~ 512
-    // int d_k = d_model, d_v = d_model; 
-    int d_k = 2;
-    int d_v = 4;
+    int d_k = d_model, d_v = d_model; 
     AttentionLayer<float> attn{d_model, d_k, d_v, on_gpu};
-    attn.init_weights_for_test(); 
+    attn.init_uniform(); // attn.parameters(); ???
     attn.forward(X, attnOutput);
     std::cout << "X= \n" << X.str() << std::endl;
     std::cout << "output= \n" << attnOutput.str() << std::endl;
@@ -63,18 +73,31 @@ void test_multiHeadAttn(const Tensor<float> X, Tensor<float> attnOutput, int d_m
     // Multihead Attention parameters
     // For multi headed attention, d_model ~ 512 and d_k, d_v are d_model / num_of_heads 
     int d_k = d_model/heads, d_v = d_model/heads; 
-    Tensor<float> attnOutputPerHead(X.h, d_model, true);
+    Tensor<float> attnOutputPerHead(X.h, d_model/heads, true); 
+    Tensor<float> attnOutputHost(attnOutput.h, attnOutput.w, false);  
+    Tensor<float> attnOutputPerHeadHost(attnOutputPerHead.h, attnOutputPerHead.w, false);  
 
-    for(int i = 0; i < heads; ++i) {
+    // attnOutputPerHead has shape (seq_length, d_model/heads)
+    // attnOutput has shape (seq_length, d_model)
+
+    for(int i = 0; i < heads; ++i) { 
         AttentionLayer<float> attn{d_model, d_k, d_v, on_gpu};
         attn.init_uniform(); // attn.parameters(); ???
         attn.forward(X, attnOutputPerHead);
         std::cout << "X= \n" << X.str() << std::endl;
         std::cout << "output= \n" << attnOutputPerHead.str() << std::endl;
-        // concatenate attnOutput to attnOutput
+        // concatenate attnOutputPerHead to attnOutput
+        attnOutputPerHead.toHost(attnOutputPerHeadHost);
 
+        for(int row = 0; row < X.h; row++) {
+            for(int col = i*(d_model/heads); col < (i+1)*(d_model/heads); col++) {
+                //std::cout << "access: " << Index(attnOutputPerHead, row, col - (i*(d_model/heads))) << std::endl; 
+                Index(attnOutputHost, row, col) = Index(attnOutputPerHeadHost, row, col - (i*(d_model/heads)));
+            }
+        }
     }
-
+    
+    attnOutputHost.toDevice(attnOutput);
 }
 
 
@@ -129,25 +152,46 @@ int main() {
     assert(op_allclose(attnOutputTorch, attnOutput));
     std::cout << "Single head attention tested! " << std::endl; 
 
-    // // Test mutli head attention : incomplete
-    // //test_multiHeadAttn(X, attnOutput, d_model, heads);
-    // //std::cout << "Multi head attention tested! " << std::endl; 
-    // // INCOMPLETE MULTI HEAD ATTENTION, DO NOT USE RIGHT NOW
+    // Attention variables 
+    int seq_length = 2; 
+    int d_model = 512, heads = 8; 
+    float scale_factor = 1.0 / std::sqrt(d_model);
 
-    // // Add & Norm
-    // op_add(X, attnOutput, X);
-    // // INCOMPLETE: need to write layer normalization 
+    // Feedforward variables
+    int n_layers = 2;
+    std::vector<int> layer_dims;
+    int d_ff = 2048;
 
-    // // Feedforward Layer: 2 layer mlp with d_model => d_ff => d_model and relu in between 
-    // for (int i = 0; i < n_layers - 1; i++)
-    // {
-    //     layer_dims.push_back(d_ff);
-    // }
-    // layer_dims.push_back(d_model); // last layer's out dimension is always 10 (# of digits) // 
+    // Initialize the embedding vectors for a sequence 
+    Tensor<float> X(seq_length, d_model, true); 
+    op_uniform_init(X, -1.0f, 1.0f);
 
-    // Tensor<float> X_ffd(seq_length, d_model, true); 
-    // MLP<float> mlp(seq_length, d_model, layer_dims, on_gpu);
-    // mlp.init();
-    // mlp.forward(X, X_ffd);
-    return 0;
+    // Test single head attention
+    Tensor<float> attnOutput(X.h, d_model, true);
+    test_singleHeadAttn(X, attnOutput, d_model);
+    std::cout << "Single head attention tested! " << std::endl; 
+
+    // Test multi head attention
+    test_multiHeadAttn(X, attnOutput, d_model, heads);
+    std::cout << "Multi head attention tested! " << std::endl; 
+
+    // Add & Norm
+    Tensor<float> addNormOutput(X.h, d_model, true);
+    op_add(X, attnOutput, addNormOutput);
+    op_layernorm(addNormOutput, addNormOutput);
+    // INCOMPLETE: need to write layer normalization 
+
+    // Feedforward Layer: 2 layer mlp with d_model => d_ff => d_model and relu in between 
+    for (int i = 0; i < n_layers - 1; i++)
+    {
+        layer_dims.push_back(d_ff);
+    }
+    layer_dims.push_back(d_model); // last layer's out dimension is always 10 (# of digits) // 
+
+    Tensor<float> outputFfd(seq_length, d_model, true); 
+    MLP<float> mlp(seq_length, d_model, layer_dims, on_gpu);
+    mlp.init();
+    mlp.forward(addNormOutput, outputFfd);
+
+    return 0; 
 }
