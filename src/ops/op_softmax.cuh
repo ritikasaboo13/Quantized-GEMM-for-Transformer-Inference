34219648c1c6eff56138f8fc649a3616110d83da
Ritika Saboo <115175278+rss9311@users.noreply.github.com>
2024-05-15 20:08:06 -0400

    update softmax kernel

--- src/ops/op_softmax.cuh
+++ src/ops/op_softmax.cuh
@@ -4,51 +4,7 @@
 #include <cmath> 
 
 template <typename T>
-__global__ void softmax_kernel(const Tensor<T> A, Tensor<T> B, int batch_size, int num_classes)
-{
-    int row = blockIdx.x; // 0, 1, .... seq_length-1
-    if (row >= batch_size) return; // batch_size = seq_length
-
-    T max_logit = Index(A, row, 0); 
-    for (int i = 1; i < num_classes; i++) {
-        if (Index(A, row, i) > max_logit) {
-            max_logit = Index(A, row, i);
-        }
-    }
-
-    T sum_exp = 0.0;
-    for (int i = 0; i < num_classes; i++) {
-        Index(B, row, i) = exp(Index(A, row, i) - max_logit);
-        sum_exp += exp(Index(A, row, i) - max_logit);
-    }
-
-    for (int i = 0; i < num_classes; i++) {
-        Index(B, row, i) = Index(B, row, i) / sum_exp;
-    }
-}
-
-template <typename T>
-void op_softmax(const Tensor<T> &A, Tensor<T> &B)
-{
-    assert(A.h == B.h && A.w == B.w);
-    assert(A.on_device && B.on_device); 
-
-    int batch_size = A.h;
-    int num_classes = A.w;
-
-    int blocksPerGrid = batch_size; // seq_length
-    int threadsPerBlock = num_classes; // seq_length
-
-    // why launch a kernel with blocksPerGrid, threadsPerBlock = seq_length x seq_length ??? 
-    // this is wasteful because only the first thread of each block is used and rest remain unused
-    // also, assuming seq_lengths are of 2048 sizes, is it right to launch 2048 blocks with 2048 threads?  
-    // there might be limits to doing so? 
-    softmax_kernel<<<blocksPerGrid, threadsPerBlock>>>(A, B, batch_size, num_classes);
-}
-
- // ===================================== // 
-template <typename T>
-__global__ void softmax_kernel_new(const Tensor<T> A, Tensor<T> B, int h, int w)
+__global__ void softmax_kernel(const Tensor<T> A, Tensor<T> B, int h, int w)
 {
     int row = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -73,7 +29,7 @@ __global__ void softmax_kernel_new(const Tensor<T> A, Tensor<T> B, int h, int w)
 }
 
 template <typename T>
-void op_softmax_new(const Tensor<T> &A, Tensor<T> &B)
+void op_softmax(const Tensor<T> &A, Tensor<T> &B)
 {
     assert(A.h == B.h && A.w == B.w);
     assert(A.on_device && B.on_device); 
